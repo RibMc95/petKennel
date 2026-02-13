56b7ed4297aacdd65bcb32a029c698c6918c1147
Micah Cole <129923155+RibMc95@users.noreply.github.com>
2026-02-13 03:07:09 +0000

    made where only one picture is shown at a time chnages to data to add status chnaged background color

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const initialData = [
   {
     id: 1,
-    name: "Baby Yoda",
-    status: "Hungry",
-    image: "https://some-url.com/sad-yoda.jpg"
+    name: "Happy Dog",
+    status: "Happy",
+    image: "https://static.wixstatic.com/media/3d4f8e_116a2fab5a3e4826b52c7d038f02534d~mv2.png/v1/fill/w_594,h_594,al_c,lg_1,q_85,enc_avif,quality_auto/3d4f8e_116a2fab5a3e4826b52c7d038f02534d~mv2.png"
   },
   {
     id: 2,
-    name: "Mandalorian",
-    status: "Lost",
-    image: "https://some-url.com/lost-mando.jpg"
+    name: "Sad Dog",
+    status: "Sad",
+    image: "https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/2/2020/03/how-can-i-tell-my-dog-is-sad-2-1-768x578.jpg"
   }
 ];
